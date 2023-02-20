1bac90659019c8d9f51a41c400b22f1d623ab6c7
Brian Wahungu <cepteinhawks@gmail.com>
2023-02-21 00:46:45 +0300

    fourth commit

--- article.rb
+++ article.rb
@@ -1,3 +1,17 @@
+require 'pry'
+class Article
+attr_reader :author, :magazine, :title
+def initialize (author, magazine, title)
+    @author = author
+    @magazine = magazine
+    @title =title
+end
+end
 
+Article.new('Elon Musk Buys Twitter', 'Best', 'Brian Wahungu')
+
+
+
+binding.pry
 
 
--- magazine.rb
+++ magazine.rb
@@ -1,11 +1,9 @@
 # TODO- INITIALIZE MAGAZINE WITH A NAME AND CATEGORY AS A STRING
-
 require 'pry'
 class Magazine
-attr_accessor :name
-attr_accessor :category
+attr_accessor :name, :category
 
-def initialize (name, category = Tech News)
+def initialize (name, category)
     @name = name
     @category = category
 end
@@ -13,6 +11,8 @@ end
 end
 
 # TODO- RETURNS THE NAME OF THE MAGAZINE AND CATEGORY OF THE MAGAZINE
-r1 = Magazine.new('Wall Street Journal', 'Tech News')
+a = Magazine.new("Best", "Tech")
+puts a.name + a.category
 
 binding.pry
+
